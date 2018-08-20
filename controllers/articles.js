7a15c75aa73d72cf0b06ad88896a6db6e399740e
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-08-20 16:47:59 -0700

    added forms for creating a new author and creating a new article

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.send('Hello World articles');
+	res.render('articles/index');
+})
+
+router.get('/new', function(req, res){
+	res.render('articles/new');
+});
+
+router.get('/:id', function(req, res){
+	res.render('articles show page goes here');
+});
+
+router.post('/', function(req, res){
+	res.send('/articles post route reached')
 })
 
 module.exports = router; 
